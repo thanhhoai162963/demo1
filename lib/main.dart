1fa7d4de1552618e917c0a7146f0afc13d17750b
thanhhoai162963 <hoaithanh249@gmail.com>
2021-04-15 20:15:10 +0700

    list set map

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,19 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+void main(){
+  // /*var list = [1,2,3,4]
+  //     ..add(5)
+  //     ..remove(2);
+  // print(list);
+  // List<Object> listObject = [1,2,3,4,"thanh"]
+  //           ..add(12.3)
+  //           ..forEach((element) {print(element);});
+  // //mang hang
+  // var listConst = const [12.3,123.5];
+  // //add theo dieu kien*/
+  // var number = 1;
+  // var list = [1,2,3,4,if(number == 1) 5];
+  // print(list);
+  var set = {23,10,5,5,5};
+  print(set);
+  var map = {"thanh":"25"}
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    // String123
-    // String name = "thanh";
-    // String name1 = "thanh";
-    // if (name.isNotEmpty);
-    // if (name  == name1);
-    // StringBuffer buffer = StringBuffer();
-    // buffer.write("a");
-    // buffer.write("a");
-    // buffer.write("a");
-    // String a = '''
-    // asdsadsa
-    // asdsadas
-    // asdsada
-    // ''';
-    var age = null;
-    age ??= 25;
-    if (age is int) {
-      age = 26;
-    }
-    if (age is! String) {
-      age = 24;
-    }
-    bool result = age == 24 ? true : false;
-    StringBuffer stringBuffer = StringBuffer()
-    ..write("object")
-    ..write("object");
-    int a =123;
-    double c = 44.5;
-    double d = a as double;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-              padding: EdgeInsets.all(20),
-              child: Text('$stringBuffer',
-                  style: TextStyle(fontSize: 25, color: Colors.white)),
-              color: Colors.pink),
-        ),
-      ),
-    );
-  }
 }
