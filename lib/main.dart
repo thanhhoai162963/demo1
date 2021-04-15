7381ae9e8ed9b6f52d61b71009b02b6f91f9ed3e
thanhhoai162963 <hoaithanh249@gmail.com>
2021-04-15 19:29:14 +0700

    function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
+void main(){
+  runApp(MyApp());
+  optionalParameter();
+  defaultParameter(name:"thanh", age: 24);
+}
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -11,46 +13,24 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    // String123
-    // String name = "thanh";
-    // String name1 = "thanh";
-    // if (name.isNotEmpty);
-    // if (name  == name1);
-    // StringBuffer buffer = StringBuffer();
-    // buffer.write("a");
-    // buffer.write("a");
-    // buffer.write("a");
-    // String a = '''
-    // asdsadsa
-    // asdsadas
-    // asdsada
-    // ''';
-    var age = null;
-    age ??= 25;
-    if (age is int) {
-      age = 26;
-    }
-    if (age is! String) {
-      age = 24;
-    }
-    bool result = age == 24 ? true : false;
-    StringBuffer stringBuffer = StringBuffer()
-    ..write("object")
-    ..write("object");
-    int a =123;
-    double c = 44.5;
-    double d = a as double;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-              padding: EdgeInsets.all(20),
-              child: Text('$stringBuffer',
-                  style: TextStyle(fontSize: 25, color: Colors.white)),
-              color: Colors.pink),
-        ),
-      ),
-    );
+
+    return Container();
   }
 }
+
+void optionalParameter(@required String email, {String name, int age}){
+  print(email);
+  print(name);
+  print(age);
+}
+void potionalParameter([@required String email, String name, int age]){
+  print(email);
+  print(name);
+  print(age);
+}
+void defaultParameter({String email="thanh thanh", String name, int age}){
+  print(email);
+  print(name);
+  print(age);
+}
+void lambdaFunction() => print("123");
